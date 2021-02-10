fc95a0e564c473b8b36fdeb6e45e33771afff3c9
Lucas Koehler <lkoehler@eclipsesource.com>
2021-02-10 17:34:00 +0100

    Remove JsonForms Redux
    * Update JsonForms dependencies to ^2.5.0
    * Remove redux and react-redux depenencies
    * Adapt detail form widget to use plain react JsonForms without redux
    
    Fix #7

--- theia-tree-editor/src/browser/detail-form-widget.tsx
+++ theia-tree-editor/src/browser/detail-form-widget.tsx
@@ -8,12 +8,11 @@
  *
  * SPDX-License-Identifier: EPL-2.0 OR MIT
  ********************************************************************************/
-import { Actions, jsonformsReducer, JsonFormsState, JsonFormsSubStates } from '@jsonforms/core';
-import { JsonFormsDispatch, JsonFormsReduxContext } from '@jsonforms/react';
+import { JsonFormsCore } from '@jsonforms/core';
+import { JsonForms } from '@jsonforms/react';
 import {
-    registerStyles,
-    StyleDef,
-    stylingReducer,
+    JsonFormsStyleContext,
+    StyleContext,
     vanillaCells,
     vanillaRenderers,
     vanillaStyles
@@ -23,8 +22,6 @@ import { BaseWidget, Message } from '@theia/core/lib/browser';
 import { inject, injectable } from 'inversify';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { AnyAction, CombinedState, combineReducers, createStore, Store } from 'redux';
 
 import { TreeEditor } from './interfaces';
 
@@ -36,96 +33,55 @@ const JSON_FORMS_CONTAINER_CSS_CLASS = 'jsonforms-container';
 @injectable()
 export class DetailFormWidget extends BaseWidget {
     private selectedNode: TreeEditor.Node;
-    private store: any;
+    private jsonformsOnChange: (state: Pick<JsonFormsCore, 'data' | 'errors'>) => void;
 
     protected changeEmitter = new Emitter<Readonly<any>>();
 
     constructor(@inject(TreeEditor.ModelService) private readonly modelService: TreeEditor.ModelService) {
         super();
 
-        this.store = this.initStore();
-        this.store.dispatch(Actions.init({}, { type: 'string' }));
         this.toDispose.push(this.changeEmitter);
-        this.store.subscribe(() => {
-            this.changeEmitter.fire(this.store.getState().jsonforms.core.data);
-        });
+        this.jsonformsOnChange = (state: Pick<JsonFormsCore, 'data' | 'errors'>) =>
+            this.changeEmitter.fire(state.data);
         this.renderEmptyForms();
     }
+
     get onChange(): Event<Readonly<any>> {
         return this.changeEmitter.event;
     }
 
-    private initStore(): Store<CombinedState<{ jsonforms: JsonFormsSubStates }>, AnyAction> {
-        const initState: JsonFormsState = {
-            jsonforms: {
-                cells: vanillaCells,
-                renderers: vanillaRenderers,
-                styles: this.createStyles(),
-                config: {
-                    restrict: false,
-                    trim: false,
-                    showUnfocusedDescription: true,
-                    hideRequiredAsterisk: false
-                }
-            }
-        };
-        return createStore(
-            combineReducers({ jsonforms: jsonformsReducer({ styles: stylingReducer }) }),
-            initState
-        );
-    }
-
-    /** Augments the default vanilla styles with theia-specific styling and returns the result. */
-    private createStyles(): StyleDef[] {
-        const registerStylesAction = registerStyles([
-            {
-                name: 'array.button',
-                classNames: ['theia-button']
-            },
-            {
-                name: 'array.table.button',
-                classNames: ['theia-button']
-            },
-            {
-                name: 'control.input',
-                classNames: ['theia-input']
-            },
-            {
-                name: 'control.select',
-                classNames: ['theia-select']
-            }
-        ]);
-        return stylingReducer(vanillaStyles, registerStylesAction);
-    }
-
     setSelection(selectedNode: TreeEditor.Node): void {
         this.selectedNode = selectedNode;
-
-        this.store.dispatch(
-            Actions.init(
-                this.modelService.getDataForNode(this.selectedNode),
-                this.modelService.getSchemaForNode(this.selectedNode),
-                this.modelService.getUiSchemaForNode(this.selectedNode),
-                {
-                    refParserOptions: {
-                        dereference: { circular: 'ignore' }
-                    },
-                    useDefaults: true
-                }
-            )
-        );
         this.renderForms();
     }
 
     protected renderForms(): void {
         if (this.selectedNode) {
+            const data = this.modelService.getDataForNode(this.selectedNode);
+            const schema = this.modelService.getSchemaForNode(this.selectedNode);
+            const uiSchema = this.modelService.getUiSchemaForNode(this.selectedNode);
+
             ReactDOM.render(
                 <div className={JSON_FORMS_CONTAINER_CSS_CLASS}>
-                    <Provider store={this.store}>
-                        <JsonFormsReduxContext>
-                            <JsonFormsDispatch />
-                        </JsonFormsReduxContext>
-                    </Provider>
+                    <JsonFormsStyleContext.Provider value={styleContextValue}>
+                        <JsonForms
+                            data={data}
+                            schema={schema}
+                            uischema={uiSchema}
+                            cells={vanillaCells}
+                            renderers={vanillaRenderers}
+                            onChange={this.jsonformsOnChange}
+                            config={{
+                                restrict: false,
+                                trim: false,
+                                showUnfocusedDescription: true,
+                                hideRequiredAsterisk: false
+                            }}
+                            refParserOptions={{
+                                dereference: { circular: 'ignore' }
+                            }}
+                        />
+                    </JsonFormsStyleContext.Provider>
                 </div>,
                 this.node
             );
@@ -133,14 +89,44 @@ export class DetailFormWidget extends BaseWidget {
             this.renderEmptyForms();
         }
     }
+
     protected renderEmptyForms(): void {
         ReactDOM.render(
             <React.Fragment>Please select an element</React.Fragment>,
             this.node
         );
     }
+
     protected onUpdateRequest(msg: Message): void {
         super.onUpdateRequest(msg);
         this.renderForms();
     }
 }
+
+// Default vanilla styles extend with theia-specific styling
+const styleContextValue: StyleContext = {
+    styles: [
+        ...vanillaStyles,
+        {
+            name: 'array.button',
+            classNames: ['theia-button']
+        },
+        {
+            name: 'array.table.button',
+            classNames: ['theia-button']
+        },
+        {
+            name: 'control.input',
+            classNames: ['theia-input']
+        },
+        {
+            name: 'control.select',
+            classNames: ['theia-select']
+        },
+        {
+            name: 'vertical.layout',
+            classNames: ['theia-vertical']
+        }
+    ]
+};
+
